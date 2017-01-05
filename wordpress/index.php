5498914ad8258b18a98d25bad514c734296fb088
Suraj Nathani <surajn222@gmail.com>
2017-01-05 23:36:18 +0530

    wp

--- wordpress/index.php
+++ wordpress/index.php
@@ -1,8 +1,17 @@
 <?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-#echo $_SERVER['HTTP_HOST'];
-#echo $_SERVER['REMOTE_ADDR'];
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-phpinfo();
-
-?>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
