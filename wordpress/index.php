408d5384725d6edbe702db767463bf834de6fda5
Suraj Nathani <surajn222@gmail.com>
2017-01-05 21:08:51 +0530

    wp

--- wordpress/index.php
+++ wordpress/index.php
@@ -1,19 +1,4 @@
 <?php
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-
-echo "<html>"
-echo "<h1>"
-echo "Hello World"
-echo "</h1>"
-echo $ip
-echo "</html>"
-
+phpinfo();
 
 ?>
